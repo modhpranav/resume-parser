df186f1b9e1e409f0e0f2ac410d0893a8ec03768
Pranav Modh <modhpranav21@gmail.com>
2024-02-20 11:15:01 -0500

    [ADD] Initial Commit

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+import spacy
+import nltk
+from spacy.matcher import PhraseMatcher
+
+# load default skills data base
+from skillNer.general_params import SKILL_DB
+# import skill extractor
+from skillNer.skill_extractor_class import SkillExtractor
+
+# init skill extractor
+nlp = spacy.load("en_core_web_sm")
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+skill_extractor = SkillExtractor(nlp, SKILL_DB, PhraseMatcher)
---
+++ app/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, UploadFile, File, HTTPException, Form
+from fastapi.responses import JSONResponse
+from app.utils import extract_text_from_pdf, get_insights, get_skills
+from app import nlp
+
+app = FastAPI()
+
+
+@app.post("/parse-pdf/")
+async def parse_pdf(pdf_file: UploadFile = File(...)):
+    
+    if pdf_file.content_type != "application/pdf":
+        raise HTTPException(status_code=400, detail="File provided is not a PDF")
+    
+    try:
+        text = await extract_text_from_pdf(pdf_file)
+
+        skills = await get_skills(text)
+        
+        return JSONResponse(content={"Skills": skills}, media_type="application/json")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/analyze-match/")
+async def analyze_match(resume: UploadFile = File(...), job_description: str = Form(...)):
+    
+    if resume.content_type != "application/pdf":
+        raise HTTPException(status_code=400, detail="Resume file provided is not PDF")
+
+    try:
+        insights = await get_insights(resume, job_description)
+        return JSONResponse(content=insights, media_type="application/json")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
