a7fa82ff4e2232f3b8e16aa0a58f99d795891b1e
Pranav Modh <modhpranav21@gmail.com>
2024-02-22 15:51:31 -0500

    [ADD] Procfile for deployment

--- app/main.py
+++ app/main.py
@@ -8,23 +8,23 @@ app = FastAPI()
 
 @app.post("/parse-pdf/")
 async def parse_pdf(pdf_file: UploadFile = File(...)):
-    
     if pdf_file.content_type != "application/pdf":
         raise HTTPException(status_code=400, detail="File provided is not a PDF")
-    
+
     try:
         text = await extract_text_from_pdf(pdf_file)
 
         skills = await get_skills(text)
-        
+
         return JSONResponse(content={"Skills": skills}, media_type="application/json")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.post("/analyze-match/")
-async def analyze_match(resume: UploadFile = File(...), job_description: str = Form(...)):
-    
+async def analyze_match(
+    resume: UploadFile = File(...), job_description: str = Form(...)
+):
     if resume.content_type != "application/pdf":
         raise HTTPException(status_code=400, detail="Resume file provided is not PDF")
 
@@ -33,5 +33,3 @@ async def analyze_match(resume: UploadFile = File(...), job_description: str = F
         return JSONResponse(content=insights, media_type="application/json")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-
